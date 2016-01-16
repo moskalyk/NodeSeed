c30d16bc202e9f846ee89437375e339f3094b259
Morgan Moskalyk <morgan.moskalyk@gmail.com>
2016-01-16 00:05:27 -0500

    middle name

--- public/controllers/loginController.js
+++ public/controllers/loginController.js
@@ -3,24 +3,78 @@ MTC.controller('loginController', function($scope, $http, $location) {
     $scope.submit = function(accountID) {
         var firstName = $('#first-name').val()
         var lastName = $('#last-name').val()
+        var middleName = $('#middle-name').val()
         var domain = $('#domain').val()
         var emails = []
 
         //Simple
-        emails.push(firstName + '@'+domain)
-        emails.push(lastName + '@'+domain)
+        emails.push(firstName + '@' + domain)
+        emails.push(lastName + '@' + domain)
 
         //Basics
-        emails.push(firstName + lastName + '@'+domain)
-        emails.push(firstName + '.' +lastName + '@'+domain)
-        emails.push(firstName.substring(0,1) + lastName + '@'+domain)
-        emails.push(firstName.substring(0,1) + '.' + lastName + '@'+domain)
-        emails.push(firstName + lastName.substring(0,1) + '@'+domain)
-        emails.push(firstName + '.' +lastName.substring(0,1) + '@'+domain)
-        emails.push(firstName.substring(0,1) + lastName.substring(0,1) + '@'+domain)
-        emails.push(firstName.substring(0,1) + '.' + lastName.substring(0,1) + '@'+domain)
+        emails.push(firstName + lastName + '@'+ domain)
+        emails.push(firstName + '.' + lastName + '@' + domain)
+        emails.push(firstName.substring(0,1) + lastName + '@' + domain)
+        emails.push(firstName.substring(0,1) + '.' + lastName + '@' + domain)
+        emails.push(firstName + lastName.substring(0,1) + '@' + domain)
+        emails.push(firstName + '.' +lastName.substring(0,1) + '@' + domain)
+        emails.push(firstName.substring(0,1) + lastName.substring(0,1) + '@' + domain)
+        emails.push(firstName.substring(0,1) + '.' + lastName.substring(0,1) + '@' + domain)
+        
+        var temp = firstName
+        firstName = lastName
+        firstName = temp
 
         //Backwards
+        emails.push(firstName + lastName + '@'+ domain)
+        emails.push(firstName + '.' + lastName + '@' + domain)
+        emails.push(firstName.substring(0,1) + lastName + '@' + domain)
+        emails.push(firstName.substring(0,1) + '.' + lastName + '@' + domain)
+        emails.push(firstName + lastName.substring(0,1) + '@' + domain)
+        emails.push(firstName + '.' +lastName.substring(0,1) + '@' + domain)
+        emails.push(firstName.substring(0,1) + lastName.substring(0,1) + '@' + domain)
+        emails.push(firstName.substring(0,1) + '.' + lastName.substring(0,1) + '@' + domain)
+        
+        var temp = firstName
+        firstName = lastName
+        firstName = temp
+
+        //Middle Names
+        emails.push(firstName.substring(0,1) + middleName.substring(0,1) + lastName + '@'+ domain)
+        emails.push(firstName + middleName.substring(0,1) + lastName + '@'+ domain)
+        emails.push(firstName + middleName.substring(0,1) + '.' + lastName + '@'+ domain)
+        emails.push(firstName + '.' + middleName.substring(0,1) + '.' + lastName + '@'+ domain)
+        emails.push(firstName + middleName + lastName + '@'+ domain)
+        emails.push(firstName + '.' + middleName + '.' +lastName + '@'+ domain)
+        
+        //Dashes
+        emails.push(firstName + '-' +lastName + '@'+ domain)
+        emails.push(firstName.substring(0,1) + '-' +lastName + '@'+ domain)
+        emails.push(firstName + '-' +lastName.substring(0,1) + '@'+ domain)
+        emails.push(firstName.substring(0,1) + '-' + lastName.substring(0,1) + '@'+ domain)
+
+        emails.push(lastName + '-' + firstName.substring(0,1) + '@'+ domain)
+        emails.push(lastName.substring(0,1) + '-' + firstName + '@'+ domain)
+        emails.push(lastName.substring(0,1) + '-' + firstName.substring(0,1) + '@'+ domain)
+        
+        // emails.push(lastName + '-' + middleName.substring(0,1) + '-' + firstName + '@'+ domain)
+        // emails.push(lastName.substring(0,1) + '-' + middleName + '-' + firstName.substring(0,1) + '@'+ domain)
+        // emails.push(lastName+ '-' + middleName + '-' + firstName + '@'+ domain)
+       
+        //Underscores
+        emails.push(firstName + '_' +lastName + '@'+ domain)
+        emails.push(firstName.substring(0,1) + '_' +lastName + '@'+ domain)
+        emails.push(firstName + '_' +lastName.substring(0,1) + '@'+ domain)
+        emails.push(firstName.substring(0,1) + '_' + lastName.substring(0,1) + '@'+ domain)
+
+        emails.push(lastName + '_' + firstName.substring(0,1) + '@'+ domain)
+        emails.push(lastName.substring(0,1) + '_' + firstName + '@'+ domain)
+        emails.push(lastName.substring(0,1) + '_' + firstName.substring(0,1) + '@'+ domain)
+        
+        // emails.push(lastName + '_' + middleName.substring(0,1) + '_' + firstName + '@'+ domain)
+        // emails.push(lastName.substring(0,1) + '_' + middleName + '_' + firstName.substring(0,1) + '@'+ domain)
+        // emails.push(lastName+ '_' + middleName + '_' + firstName + '@'+ domain)
+
 
         var masterEmailList = ''
 
